c12fe6740bb221e36fa30aaca2f96ed1c3b20faf
nickdferrara <nick.d.ferrara@gmail.com>
2024-01-02 21:18:04 -0500

    Initial commit

---
+++ src/main/kotlin/com/nickdferrara/departmentservice/DepartmentserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.nickdferrara.departmentservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DepartmentserviceApplication
+
+fun main(args: Array<String>) {
+    runApplication<DepartmentserviceApplication>(*args)
+}
---
+++ src/test/kotlin/com/nickdferrara/departmentservice/DepartmentserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nickdferrara.departmentservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DepartmentserviceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
