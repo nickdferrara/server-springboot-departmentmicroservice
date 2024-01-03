77531c09ed6d5629839e92d7cd1d3c8cd612dfeb
nickdferrara <nick.d.ferrara@gmail.com>
2024-01-02 22:03:00 -0500

    Move service config to the server config

--- src/main/kotlin/com/nickdferrara/departmentservice/DepartmentserviceApplication.kt
+++ src/main/kotlin/com/nickdferrara/departmentservice/DepartmentserviceApplication.kt
@@ -2,8 +2,10 @@ package com.nickdferrara.departmentservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class DepartmentserviceApplication
 
 fun main(args: Array<String>) {
